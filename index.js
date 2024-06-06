afe6085ea900d08da1dca316fe8b4d0f4088eecf
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-06-06 10:05:55 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = +req.params.id;
+
+  const note = notes.find((n) => n.id === id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = +req.params.id;
+
+  const oldLength = notes.length;
+  notes = notes.filter((n) => n.id !== id);
+
+  if (notes.length === oldLength) {
+    res.status(404).end();
+  } else {
+    res.status(204).end();
+  }
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (req, res) => {
+  const { content, important } = req.body;
+
+  if (!content) {
+    return res.status(400).end();
+  }
+
+  const newNote = {
+    id: generateId(),
+    content,
+    important: Boolean(important) ?? false,
+  };
+
+  notes.push(newNote);
+
+  res.status(201).json(newNote);
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
